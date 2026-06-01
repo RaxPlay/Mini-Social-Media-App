3728d794a6119e089f1a66d9552dbfee9faba055
Raxon Suri <raxonsuriperez@gmail.com>
2026-06-01 15:58:34 -0400

    Settings

--- Mini-Social-Media-App/vite.config.js
+++ Mini-Social-Media-App/vite.config.js
@@ -10,4 +10,14 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
+  server: {
+	    proxy: {
+		    "/api": {
+			    target: "http://localhost:5000",
+		    },
+        "/post": {
+          target: "http://localhost:5000",
+        }
+	    }
+    },
 })
