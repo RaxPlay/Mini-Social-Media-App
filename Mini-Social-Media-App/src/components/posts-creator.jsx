56a4b5d759219d0c0c487e5e6dcdeed29c2af817
Raxon Suri <raxonsuriperez@gmail.com>
2026-06-01 22:08:32 -0400

    Added functionality to create posts and changed some styles

--- Mini-Social-Media-App/src/components/posts-creator.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const PostsCreator = () => {
-  return (
-    <div id='container' >
-      hola
-    </div>
-  )
-}
--- Mini-Social-Media-App/src/pages/home.jsx
+++ Mini-Social-Media-App/src/pages/home.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 import { NavBar } from '../components/v-navbar'
-import { PostsCreator } from '../components/posts-creator'
 import { PostsDisplay } from '../components/posts-display'
 
 export const Home = ({ user, setUser }) => {
+  const [postForm, setPostForm] = useState({
+    post_description: "",
+    like_count: 0,
+  });
+
+  const newPost = async(e) => {
+    e.preventDefault();
+    try {
+      const newPost = await axios.post("/post/new-post", postForm);
+      console.log(newPost)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className='flex justify-center h-screen'>
       <div id='home-display'>
-        <PostsCreator></PostsCreator>
+        {user ? (
+        <div
+          id="container"
+          className="rounded-b-md p-3"
+        >
+          <form className="flex justify-center gap-3" onSubmit={newPost}>
+            <input
+              type="text"
+              placeholder="What are you thinking about?"
+              value={postForm.post_description}
+              onChange={(e) => {
+                setPostForm({ ...postForm, post_description: e.target.value });
+              }}
+              className="w-57"
+            />
+            <button onClick={newPost}>
+              <i className="fa-solid fa-plus"></i>
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div></div>
+      )}
       </div>
     </div>
   )
